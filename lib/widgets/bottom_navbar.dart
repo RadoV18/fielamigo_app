d41cd419adcdb9798d0f26b4c267c76d59154e66
iguiIllanes <igui10082002@gmail.com>
2022-10-19 12:44:06 -0400

    Add template for home screen

---
+++ lib/widgets/bottom_navbar.dart
@@ -0,0 +1,41 @@
+import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
+import 'package:circular_bottom_navigation/tab_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/bottom_navbar_cubit/bottom_navbar_cubit.dart';
+import '../bloc/bottom_navbar_cubit/bottom_navbar_state.dart';
+
+class Template extends StatelessWidget {
+  int startingIndex;
+  List<TabItem> tabItems;
+  List screens;
+
+  Template({super.key, this.startingIndex = 0, required this.tabItems, required this.screens});
+
+  final CircularBottomNavigationController _navigationController =
+      CircularBottomNavigationController(2);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: BlocBuilder<BottomNavBarCubit, BottomNavBarState>(
+          builder: (context, state) {
+            return screens[state.currentIndex];
+          },
+        ),
+        bottomNavigationBar: Padding(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
+          child: BlocBuilder<BottomNavBarCubit, BottomNavBarState>(
+            builder: (context, state) => CircularBottomNavigation(
+              tabItems,
+              controller: _navigationController,
+              selectedCallback: ((selectedPos) =>
+                  BlocProvider.of<BottomNavBarCubit>(context)
+                      .setIndex(selectedPos!)),
+            ),
+          ),
+        ));
+  }
+}
