d2f43914e6e9cafa48a3cb9f75adb3ca53d72c91
iguiIllanes <igui10082002@gmail.com>
2022-10-19 12:44:52 -0400

    Modify home screen

--- lib/screens/caregiver_home/caregiver_home_screen.dart
+++ lib/screens/caregiver_home/caregiver_home_screen.dart
@@ -1,14 +1,7 @@
-import 'dart:developer';
-
-import 'package:fielamigo_app/screens/caregiver_test/c_test.dart';
-import 'package:flutter/material.dart';
-
-import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
 import 'package:circular_bottom_navigation/tab_item.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../bloc/bottom_navbar_cubit/bottom_navbar_cubit.dart';
-import '../../bloc/bottom_navbar_cubit/bottom_navbar_state.dart';
+import 'package:fielamigo_app/screens/caregiver_test/c_test.dart';
+import 'package:fielamigo_app/widgets/bottom_navbar.dart';
+import 'package:flutter/material.dart';
 
 class CaregiverHomeScreen extends StatelessWidget {
   CaregiverHomeScreen({super.key});
@@ -21,34 +14,25 @@ class CaregiverHomeScreen extends StatelessWidget {
   ]);
 
   List screens = [
-    const CareTest(st: 'Uno',),
-    const CareTest(st: "Dos",),
-    const CareTest(st: "Tres",),
-    const CareTest(st: "Cuatro",),
-    const CareTest(st: "Cinco",),
+    const CareTest(
+      st: 'Uno',
+    ),
+    const CareTest(
+      st: "Dos",
+    ),
+    const CareTest(
+      st: "Tres",
+    ),
+    const CareTest(
+      st: "Cuatro",
+    ),
+    const CareTest(
+      st: "Cinco",
+    ),
   ];
-
-  final CircularBottomNavigationController _navigationController =
-      CircularBottomNavigationController(2);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<BottomNavBarCubit, BottomNavBarState>(
-        builder: (context, state) {
-          return screens[state.currentIndex];
-        },
-      ),
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
-          child: BlocBuilder<BottomNavBarCubit, BottomNavBarState>(
-            builder: (context, state) => CircularBottomNavigation(
-              tabItems,
-              controller: _navigationController,
-              selectedCallback: ((selectedPos) => BlocProvider.of<BottomNavBarCubit>(context).setIndex(selectedPos!)),
-            ),
-          ),
-        )
-    );
+    return Template(startingIndex: 2,tabItems: tabItems, screens: screens);
   }
 }
