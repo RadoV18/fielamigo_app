74f04e2cfbec450fbaea67b820e64bfc5e752708
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-11-06 19:11:08 -0400

    Fixed expanded column with CustomScrollView

--- lib/screens/profile_personal_information/profile_personal_information.dart
+++ lib/screens/profile_personal_information/profile_personal_information.dart
@@ -36,84 +36,88 @@ class _ProfilePersonalInformationScreenState extends State<ProfilePersonalInform
             overscroll.disallowIndicator();
             return false;
           },
-          child: SingleChildScrollView (
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // Profile picture with button
-                    Column(
+          child: CustomScrollView (
+            slivers: [
+              SliverFillRemaining(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const SizedBox(height: 20),
-                        CustomImagePicker(
-                          enabled: canEdit,
-                          // imagePath: context.read<PersonalInformationCubit>().state.image!.path,
-                          // onImageSelected: (image) => context.read<PersonalInformationCubit>().setImage(image),
-                          imagePath: "",
-                          onImageSelected: (image) => print(""),
+                        // Profile picture with button
+                        Column(
+                          children: [
+                            const SizedBox(height: 20),
+                            CustomImagePicker(
+                              enabled: canEdit,
+                              // imagePath: context.read<PersonalInformationCubit>().state.image!.path,
+                              // onImageSelected: (image) => context.read<PersonalInformationCubit>().setImage(image),
+                              imagePath: "",
+                              onImageSelected: (image) => print(""),
+                            ),
+                            // Name
+                            TextField(
+                              enabled: canEdit,
+                              decoration: const InputDecoration(
+                                labelText: "Nombre"
+                              ),
+                              // onChanged: (value) => context.read<PersonalInformationCubit>().setName(value),
+                              onChanged: (value) => print(""),
+                            ),
+                            const SizedBox(height: 20),
+                            // Last name
+                            TextField(
+                              enabled: canEdit,
+                              decoration: const InputDecoration(
+                                labelText: "Apellido"
+                              ),
+                              // onChanged: (value) => context.read<PersonalInformationCubit>().setLastName(value),
+                              onChanged: (value) => print(""),
+                            ),
+                            const SizedBox(height: 20),
+                            // Phone number
+                            TextField(
+                              enabled: canEdit,
+                              decoration: const InputDecoration(
+                                labelText: "Teléfono"
+                              ),
+                              keyboardType: TextInputType.phone,
+                              // onChanged: (value) => context.read<PersonalInformationCubit>().setPhoneNumber(value),
+                              onChanged: (value) => print(""),
+                            ),
+                            const SizedBox(height: 20),
+                            // Birthdate
+                            CustomDatePicker(
+                              enabled: canEdit,
+                              // onDateChanged: (date) => context.read<PersonalInformationCubit>().setBirthdate(date),
+                              onDateChanged: (date) => print(date),
+                            ),
+                            const SizedBox(height: 20),
+                          ],
                         ),
-                        // Name
-                        TextField(
-                          enabled: canEdit,
-                          decoration: const InputDecoration(
-                            labelText: "Nombre"
-                          ),
-                          // onChanged: (value) => context.read<PersonalInformationCubit>().setName(value),
-                          onChanged: (value) => print(""),
-                        ),
-                        const SizedBox(height: 20),
-                        // Last name
-                        TextField(
-                          enabled: canEdit,
-                          decoration: const InputDecoration(
-                            labelText: "Apellido"
-                          ),
-                          // onChanged: (value) => context.read<PersonalInformationCubit>().setLastName(value),
-                          onChanged: (value) => print(""),
-                        ),
-                        const SizedBox(height: 20),
-                        // Phone number
-                        TextField(
-                          enabled: canEdit,
-                          decoration: const InputDecoration(
-                            labelText: "Teléfono"
-                          ),
-                          keyboardType: TextInputType.phone,
-                          // onChanged: (value) => context.read<PersonalInformationCubit>().setPhoneNumber(value),
-                          onChanged: (value) => print(""),
-                        ),
-                        const SizedBox(height: 20),
-                        // Birthdate
-                        CustomDatePicker(
-                          enabled: canEdit,
-                          // onDateChanged: (date) => context.read<PersonalInformationCubit>().setBirthdate(date),
-                          onDateChanged: (date) => print(date),
-                        ),
-                        const SizedBox(height: 20),
+                        // Cancel and save buttons
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text("Cancelar"),
+                            ),
+                            ElevatedButton(
+                              // TODO: save data
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text("Guardar"),
+                            ),
+                          ],
+                        )
                       ],
                     ),
-                    // Cancel and save buttons
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: const Text("Cancelar"),
-                        ),
-                        ElevatedButton(
-                          // TODO: save data
-                          onPressed: () => Navigator.pop(context),
-                          child: const Text("Guardar"),
-                        ),
-                      ],
-                    )
-                  ],
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
         )
       )
